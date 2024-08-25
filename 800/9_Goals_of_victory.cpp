3494c1245abd38d38bbcd05dde254bf98f1c0513
don-arko <116270662+don-arko@users.noreply.github.com>
2024-08-25 07:13:10 +0530

    Create 9_Goals_of_victory.cpp

---
+++ 800/9_Goals_of_victory.cpp
@@ -0,0 +1,135 @@
+/*
+      https://codeforces.com/problemset/problem/1877/A
+      
+     ॐ त्र्यम्बकं यजामहे सुगन्धिं पुष्टिवर्धनम् |
+     उर्वारुकमिव बन्धनान्मृत्योर्मुक्षीय माऽमृतात् ||
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int            long long int
+#define F              first
+#define S              second
+#define pb             push_back
+#define si             set <int>
+#define vi             vector <int>
+#define pii            pair <int, int>
+#define vpi            vector <pii>
+#define vpp            vector <pair<int, pii>>
+#define mii            map <int, int>
+#define mpi            map <pii, int>
+#define spi            set <pii>
+#define endl           "\n"
+#define sz(x)          ((int) x.size())
+#define all(p)         p.begin(), p.end()
+#define double         long double
+#define que_max        priority_queue <int>
+#define que_min        priority_queue <int, vi, greater<int>>
+#define bug(...)       __f (#__VA_ARGS__, __VA_ARGS__)
+#define print(a)       for(auto x : a) cout << x << " "; cout << endl
+#define print1(a)      for(auto x : a) cout << x.F << " " << x.S << endl
+#define print2(a,x,y)  for(int i = x; i < y; i++) cout<< a[i]<< " "; cout << endl
+
+inline int power(int a, int b)
+{
+    int x = 1;
+    while (b)
+    {
+        if (b & 1) x *= a;
+        a *= a;
+        b >>= 1;
+    }
+    return x;
+}
+
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << endl; }
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args)
+{
+    const char* comma = strchr (names + 1, ',');
+    cout.write (names, comma - names) << " : " << arg1 << " | "; __f (comma + 1, args...);
+}
+
+
+/*-------------------------------------------------------------------------------------------------*/
+
+
+
+
+const int N = 200005;
+
+/*
+Editorial
+Notice that each goal increases the efficiency of the team that scores by 1. But it also simultaneously decreases the efficiency of the opposite team by 1
+. This means, if we maintain the sum of efficiency for all teams, each goal does not change the sum. Therefore, the sum must be 0
+.
+
+In order to make the sum to be 0
+, the efficiency of the missing team must be equal to the sum of efficiency of the other n−1
+ teams, multiplied by −1
+.
+
+Time complexity for each test case: O(n)
+
+
+
+concept 
+
+--efficiency is nothing but net goal scored , -ve means net goal conceded
+
+--suppose n=2 , then only two teams are there (only 1 match) if E1= +2 => net goal goal mara hai 
+- then E2 has to be -2 kyuki usini fir net do goal khaya hoga , so summation efficiency is always 0
+- we could extend this concept for any greater value of n just think of it .
+
+*/
+
+void solve() {
+    int n;
+    cin>>n;
+    int sum=0;
+    for(int i=1;i<n;i++) {
+        int temp;
+        cin>>temp;
+        sum+=temp;
+    }
+
+    cout<<-sum<<endl;
+
+
+}
+
+
+
+/*-------------------------------------------------------------------------------------------------*/
+
+
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt",  "r",  stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    clock_t z = clock();
+
+/*-------------------------------------------------------------------------------------------------*/
+
+
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+
+    cerr << "Run Time : " << ((double)(clock() - z) / CLOCKS_PER_SEC);
+
+    return 0;
+
+
+/*-------------------------------------------------------------------------------------------------*/
+}
