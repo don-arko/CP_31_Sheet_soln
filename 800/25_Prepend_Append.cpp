574d039c272bfc14711e5ae96e88deae89db536d
Arko Barman <116270662+don-arko@users.noreply.github.com>
2024-08-31 16:03:08 +0530

    Create 25_Prepend_Append.cpp

---
+++ 800/25_Prepend_Append.cpp
@@ -0,0 +1,125 @@
+/*
+
+https://codeforces.com/problemset/problem/1791/C
+
+     ॐ त्र्यम्बकं यजामहे सुगन्धिं पुष्टिवर्धनम् |
+     उर्वारुकमिव बन्धनान्मृत्योर्मुक्षीय माऽमृतात् ||
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int            long long int
+#define F              first
+#define S              second
+#define pb             push_back
+#define si             set <int>
+#define vi             vector <int>
+#define pii            pair <int, int>
+#define vpi            vector <pii>
+#define vpp            vector <pair<int, pii>>
+#define mii            map <int, int>
+#define mpi            map <pii, int>
+#define spi            set <pii>
+#define endl           "\n"
+#define sz(x)          ((int) x.size())
+#define all(p)         p.begin(), p.end()
+#define double         long double
+#define que_max        priority_queue <int>
+#define que_min        priority_queue <int, vi, greater<int>>
+#define bug(...)       __f (#__VA_ARGS__, __VA_ARGS__)
+#define print(a)       for(auto x : a) cout << x << " "; cout << endl
+#define print1(a)      for(auto x : a) cout << x.F << " " << x.S << endl
+#define print2(a,x,y)  for(int i = x; i < y; i++) cout<< a[i]<< " "; cout << endl
+
+inline int power(int a, int b)
+{
+    int x = 1;
+    while (b)
+    {
+        if (b & 1) x *= a;
+        a *= a;
+        b >>= 1;
+    }
+    return x;
+}
+
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << endl; }
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args)
+{
+    const char* comma = strchr (names + 1, ',');
+    cout.write (names, comma - names) << " : " << arg1 << " | "; __f (comma + 1, args...);
+}
+
+
+/*-------------------------------------------------------------------------------------------------*/
+
+
+/*
+CONCEPT
+-Idea is to just reverse the procedure i.e. when both side bits are unequal then then just they could be striped off so n-=2 and go on checking further
+- and when they are equal we just cant strip so this is the initial string
+*/
+const int N = 200005;
+
+
+void solve() {
+    int n;
+    cin>>n;
+    string s;
+    cin>>s;
+    int l=0;
+    int r=n-1;
+    while(l<r){ // at all point of time I want the pointer to point at distinct elements i.e. they should not point to the same element 
+        if(s[l]!=s[r]){
+            n-=2;
+            --r;
+            ++l;
+        }
+        else{
+            break;
+        }
+    }
+    cout<<n<<endl;
+
+
+
+
+    
+}
+
+
+
+/*-------------------------------------------------------------------------------------------------*/
+
+
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt",  "r",  stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    clock_t z = clock();
+
+/*-------------------------------------------------------------------------------------------------*/
+
+
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+
+    cerr << "Run Time : " << ((double)(clock() - z) / CLOCKS_PER_SEC);
+
+    return 0;
+
+
+/*-------------------------------------------------------------------------------------------------*/
+}
